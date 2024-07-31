bf4f44055b87adc2d240dbf00eaea73dae2e2910
mkicjn <mkicjn@gmail.com>
2024-07-31 16:26:45 -0500

    Adjust definitions to use macros

--- kanren.lisp
+++ kanren.lisp
@@ -48,40 +48,36 @@
 (define var (lambda (x) x))
 (define var?  (lambda (x) (eq (type x) 'number)))
 
-(define walk
-  (lambda (v s)
-    (cond ((not (var? v)) v)
-	  (t (let ((b (assoc v s)))
-	       (cond (b (walk (cdr b) s))
-		     (t v)))))))
+(defun walk (v s)
+  (cond ((not (var? v)) v)
+	(t (let ((b (assoc v s)))
+	     (cond (b (walk (cdr b) s))
+		   (t v))))))
 
 ;(walk 0 '((2 . cat) (1 . 2) (0 . 1)))
 ;(walk 3 '((2 . cat) (1 . 2) (0 . 1)))
 
-(define occurs?
-  (lambda (x v s)
-    (cond ((var? v) (eq x v))
-	  ((pair? v)
-	   (or (occurs? x (walk (car v) s) s)
-	       (occurs? x (walk (cdr v) s) s)))
-	  (t ()))))
+(defun occurs? (x v s)
+  (cond ((var? v) (eq x v))
+	((pair? v)
+	 (or (occurs? x (walk (car v) s) s)
+	     (occurs? x (walk (cdr v) s) s)))
+	(t ())))
 
-(define ext-s
-  (lambda (x v s)
-    (cond ((occurs? x v s) ())
-	  (t (cons (cons x v) s)))))
+(defun ext-s (x v s)
+  (cond ((occurs? x v s) ())
+	(t (cons (cons x v) s))))
 
 ;(ext-s 3 'dog '((2 . cat) (1 . 2) (0 . 1)))
 
-(define unify
-  (lambda (a b s)
-    (cond ((eq a b) s)
-	  ((var? a) (ext-s a b s))
-	  ((var? b) (ext-s b a s))
-	  ((and (pair? a) (pair? b))
-	   (let ((s1 (unify (walk (car a) s) (walk (car b) s) s)))
-	     (and s1 (unify (walk (cdr a) s1) (walk (cdr b) s1) s1))))
-	  (t ()))))
+(defun unify (a b s)
+  (cond ((eq a b) s)
+	((var? a) (ext-s a b s))
+	((var? b) (ext-s b a s))
+	((and (pair? a) (pair? b))
+	 (let ((s1 (unify (walk (car a) s) (walk (car b) s) s)))
+	   (and s1 (unify (walk (cdr a) s1) (walk (cdr b) s1) s1))))
+	(t ())))
 
 ;(unify 1 0 '((2 . cat) (1 . 2) (0 . 1)))
 ;(unify 3 'dog '((2 . cat) (1 . 2) (0 . 1)))
@@ -103,31 +99,28 @@
 (define mzero ())
 (define unit (lambda (s/c) (cons s/c mzero)))
 
-(define ==
-  (lambda (a b)
-    (lambda (s/c)
-      (let ((s (car s/c)) (c (cdr s/c)))
-	(let ((s1 (unify a b s)))
-	  (cond (s1 (unit (cons s1 c)))
-		(t mzero)))))))
+(defun == (a b)
+  (lambda (s/c)
+    (let ((s (car s/c)) (c (cdr s/c)))
+      (let ((s1 (unify a b s)))
+	(cond (s1 (unit (cons s1 c)))
+	      (t mzero))))))
 
 (define init-s/c (cons mzero 0))
 
 ;(== '(2 . cat) '(1 . cat))
 ;((== '(2 . cat) '(1 . cat)) s/c0)
 
-(define call/fresh
-  (lambda (f)
-    (lambda (s/c)
-      (let ((s (car s/c)) (c (cdr s/c)))
-	((f (var c)) (cons s (+ c 1)))))))
+(defun call/fresh (f)
+  (lambda (s/c)
+    (let ((s (car s/c)) (c (cdr s/c)))
+      ((f (var c)) (cons s (+ c 1))))))
 
 ;((call/fresh (lambda (x) (== x 'cat))) init-s/c)
 
-(define disj
-  (lambda (g1 g2)
-    (lambda (s/c)
-      (mplus (g1 s/c) (g2 s/c)))))
+(defun disj (g1 g2)
+  (lambda (s/c)
+    (mplus (g1 s/c) (g2 s/c))))
 
 ;(define mplus
 ;  (lambda (stream1 stream2)
@@ -136,10 +129,9 @@
 
 ;(mplus '(1 2 3) '(4 5 6))
 
-(define conj
-  (lambda (g1 g2)
-    (lambda (s/c)
-      (bind g2 (g1 s/c)))))
+(defun conj (g1 g2)
+  (lambda (s/c)
+    (bind g2 (g1 s/c))))
 
 ;(define bind
 ;  (lambda (goal stream)
@@ -152,41 +144,36 @@
 
 (define promise? (lambda (x) (eq (type x) 'lambda)))
 
-(define delay (macro (x) (list lambda () x)))
-(define force (macro (x) (list x)))
+(defmacro delay (x) (` lambda () , x))
+(defmacro force (x) (` , x))
 
-(define mplus
-  (lambda (stream1 stream2)
-    (cond ((not stream1) stream2)
-	  ((promise? stream1) (delay (mplus stream2 (force stream1))))
-	  (t (cons (car stream1) (mplus (cdr stream1) stream2))))))
+(defun mplus (stream1 stream2)
+  (cond ((not stream1) stream2)
+	((promise? stream1) (delay (mplus stream2 (force stream1))))
+	(t (cons (car stream1) (mplus (cdr stream1) stream2)))))
 
-(define bind
-  (lambda (goal stream)
-    (cond ((not stream) ())
-	  ((promise? stream) (delay (bind goal (force stream))))
-	  (t (mplus (goal (car stream)) (bind goal (cdr stream)))))))
+(defun bind (goal stream)
+  (cond ((not stream) ())
+	((promise? stream) (delay (bind goal (force stream))))
+	(t (mplus (goal (car stream)) (bind goal (cdr stream))))))
 
 ;(force ((call/fresh (lambda (x) (lambda (s/c) (delay ((disj (== x 'cat) (== x 'dog)) s/c))))) init-s/c))
 
-(define turtles
-  (lambda (x)
-    (lambda (s/c)
-      (delay ((disj (== x 'turtle) (turtles x)) s/c)))))
+;(defun turtles (x)
+;  (lambda (s/c)
+;    (delay ((disj (== x 'turtle) (turtles x)) s/c))))
 
 ;(force ((call/fresh (lambda (x) (turtles x))) init-s/c))
 ;(force (cdr (force ((call/fresh (lambda (x) (turtles x))) init-s/c))))
 ;(force (cdr (force (cdr (force ((call/fresh (lambda (x) (turtles x))) init-s/c))))))
 
-(define pull
-  (lambda (stream)
-    (cond ((promise? stream) (pull (force stream)))
-	  (t stream))))
+(defun pull (stream)
+  (cond ((promise? stream) (pull (force stream)))
+	(t stream)))
 
-(define take
-  (lambda (n stream)
-    (cond ((= n 1) (list (car stream)))
-	  (t (cons (car stream) (take (- n 1) (pull (cdr stream))))))))
+(defun take (n stream)
+  (cond ((= n 1) (list (car stream)))
+	(t (cons (car stream) (take (- n 1) (pull (cdr stream)))))))
 
 ;(take 4 (pull ((call/fresh (lambda (x) (turtles x))) init-s/c)))
 
@@ -209,22 +196,28 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Some macros to make things a little easier
 
-(define relation
-  (macro (args body)
-	 (list lambda args
-	       (list lambda '(s/c)
-		     (list delay (list body 's/c))))))
+;(define relation
+;  (macro (args body)
+;	 (list lambda args
+;	       (list lambda '(s/c)
+;		     (list delay (list body 's/c))))))
+(defmacro relation (args body)
+  (` lambda , args
+     (lambda (s/c)
+       (delay (, body s/c)))))
 
 (define dogs (relation (x) (disj (== x 'dog) (dogs x))))
 (define cats (relation (x) (disj (== x 'cat) (cats x))))
 
-(define fresh1
-  (macro (arg body)
-	 (list call/fresh (list lambda (list arg) body))))
+;(define fresh1
+;  (macro (arg body)
+;	 (list call/fresh (list lambda (list arg) body))))
+(defmacro fresh1 (arg body)
+  (` call/fresh (lambda (, arg) , body)))
 
 ;(take 4 (pull ((fresh1 x (dogs x)) init-s/c)))
 
-(define run (lambda (n g) (take n (pull (g init-s/c)))))
+(defun run (n g) (take n (pull (g init-s/c))))
 
 ;(run 4 (fresh1 x (dogs x)))
 (run 4 (fresh1 x (disj (dogs x) (cats x))))
