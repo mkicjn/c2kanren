226f4087b7d5de01a312d535bdaf455ce2e2727d
mkicjn <mkicjn@gmail.com>
2024-08-01 20:37:54 -0500

    Add curried define example to tests/define.lisp

--- tests/define.lisp
+++ tests/define.lisp
@@ -12,4 +12,24 @@ a
 		     (list define name
 			   (list lambda args body))))
 (defun ident (x) x)
-(ident a)
+(ident t)
+
+
+; Scheme-like curried definitions
+(define list (lambda args args))
+(define Y (lambda (f) (f (lambda args ((Y f) . args)))))
+(define define+
+  (macro (args body)
+	 (cons define
+	       ((Y (lambda (expand)
+		     (lambda (args body)
+		       (cond ((not args) body)
+			     ((atom args) (list args body))
+			     (t (expand (car args) (list lambda (cdr args) body)))))))
+		args body))))
+
+(define+ (((a b c) d) e) (+ b c d e))
+(= 10 (((a 1 2) 3) 4))
+
+(define+ a 'x)
+(eq 'x a)
