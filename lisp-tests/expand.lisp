188b75151c0edcef1cfd84e385c4735d3e4fc265
mkicjn <mkicjn@gmail.com>
2025-01-13 18:22:11 -0600

    Add defun and defmacro to macro expansion test

--- lisp-tests/expand.lisp
+++ lisp-tests/expand.lisp
@@ -1,17 +1,17 @@
 ; Experiments with expanding macros at read time
 ; (i.e., to eventually do macros better than the interpreter provides)
 
-(define assoc
-  (lambda (s l)
-    (cond ((not l) ())
-	  ((eq s (car (car l))) (car l))
-	  (t (assoc s (cdr l))))))
-
 (define list (lambda args args))
 
 (define curry
   (lambda (f x)
     (lambda args (f x . args))))
+
+(define assoc
+  (lambda (s l)
+    (cond ((not l) ())
+	  ((eq s (car (car l))) (car l))
+	  (t (assoc s (cdr l))))))
 
 (define map
   (lambda (f l)
@@ -26,26 +26,50 @@
 
 (define expander
   (lambda (rules)
-    (lambda term (map (curry expandN rules) term))))
+    (lambda term (map (curry expand-all rules) term))))
 
-(define expandN
+(define expand-all
   (lambda (rules term)
     (cond ((atom term) term)
 	  (t ((pick-rule rules term (expander rules)) . term)))))
 
 
-(expandN (list (cons 'left (lambda (left x y) x))
-	       (cons 'right (lambda (right x y) y)))
-	'(cons (left a b) (right a b)))
-
+(expand-all (list (cons 'left (lambda (left x y) x))
+		  (cons 'right (lambda (right x y) y)))
+	    '(cons (left a b) (right a b)))
 
 (define expand-rules
   (list
     (cons 'left (lambda (left x y) x))
     (cons 'right (lambda (right x y) y))
+    (cons 'defun (lambda (defun name/args body)
+		   (list 'define
+			 (car name/args)
+			 (list 'lambda
+			       (cdr name/args)
+			       body))))
+    (cons 'defmacro (lambda (defmacro name/args body)
+		      (list 'define
+			    'expand-rules
+			    (list 'cons
+				  (list 'cons
+					(list 'quote (car name/args))
+					(list 'lambda name/args body))
+				  'expand-rules))))
     (cons 'quote list)
     ))
 
-(define expand (lambda (term) (expandN expand-rules term)))
+(define expand (lambda (term) (expand-all expand-rules term)))
 
 (cons (left 'a 'b) (right 'a 'b))
+
+(defun (assoc s l)
+  (cond ((not l) ())
+	((eq s (car (car l))) (car l))
+	(t (assoc s (cdr l)))))
+
+(assoc 'b '((a . 1) (b . 2) (c . 3)))
+
+(defmacro (middle a b c) b)
+
+(cons (middle 'a 'b 'c) (right 'b 'c))
