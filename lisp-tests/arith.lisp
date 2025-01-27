7d607482d18edf2ba75ceae37e42a219a2bff1b9
mkicjn <mkicjn@gmail.com>
2025-01-26 21:52:38 -0600

    lisp-tests/arith.lisp: Multiplication and decimal parsing

--- lisp-tests/arith.lisp
+++ lisp-tests/arith.lisp
@@ -1,7 +1,9 @@
 ; Experimenting with implementing arithmetic symbolically
 
-(define xor (lambda (a b) (cond (a (cond (b ()) (t t)))
-				(b (cond (a ()) (t t))))))
+(defun (none l) (cond ((not l) t) ((car l) ()) (t (none (cdr l)))))
+(defun (one l) (cond ((not l) ()) ((car l) (none (cdr l))) (t (one (cdr l)))))
+
+(define xor (lambda ls (one ls)))
 
 (define add-bit   (lambda (a b c) (xor c (xor a b))))
 (define carry-bit (lambda (a b c) (or (and (xor a b) c) (and a b))))
@@ -31,4 +33,56 @@
 
 (add-bits '(t t t t) '(t))
 
+(subtract-bits '(t t t t) '(t () () ()))
+
 (invert '(t t () t () t () ()))
+
+
+(define lsh1 (lambda (x) (cons () x)))
+(define rsh1 (lambda (x) (cdr x)))
+(define lsb (lambda (x) (car x)))
+
+(define multiply-bits-acc
+  (lambda (a b acc)
+    (cond ((not b) acc)
+	  ((lsb b) (multiply-bits-acc (lsh1 a) (rsh1 b) (add-bits a acc)))
+	  (t       (multiply-bits-acc (lsh1 a) (rsh1 b) acc)))))
+
+(define multiply-bits
+  (lambda (a b)
+    (multiply-bits-acc a b ())))
+
+(multiply-bits '(t t t) '(() t t))
+
+
+(define decimal-values
+  '((0 . ())
+    (1 . (t))
+    (2 . (() t))
+    (3 . (t t))
+    (4 . (() () t))
+    (5 . (t () t))
+    (6 . (() t t))
+    (7 . (t t t))
+    (8 . (() () () t))
+    (9 . (t () () t))
+    (10 . (() t () t))))
+
+(define decimal-bits (lambda (x) (cdr (assoc x decimal-values))))
+
+(define ten (decimal-bits '10))
+
+(define parse-decimal-acc
+  (lambda (digits acc)
+    (cond ((not digits) acc)
+	  (t (parse-decimal-acc (cdr digits)
+				(add-bits (decimal-bits (car digits))
+					  (multiply-bits acc ten)))))))
+
+(define parse-decimal (lambda (digits) (parse-decimal-acc digits ())))
+
+(parse-decimal '(1 2))
+
+(parse-decimal '(4 2))
+
+(multiply-bits (parse-decimal '(1 2 3)) (parse-decimal '(4 5 6)))
