9cb9f89d0e7b1c4b606648ad448554800c5b92e3
mkicjn <mkicjn@gmail.com>
2026-05-03 22:40:43 -0500

    literate_ukanren.lisp: Usability upgrades

--- literate_ukanren.lisp
+++ literate_ukanren.lisp
@@ -1,4 +1,9 @@
-; This Lisp dialect has some quirks related to runtime type checking,
+; (Heavily WIP)
+
+; This source code is meant to be a personal attempt at implementing and explaining μKanren from scratch,
+; so it's written in a "literate style" based on personal understanding and may deviate from the paper.
+
+; First of all, this particular Lisp has some quirks related to runtime type checking,
 ; so we start with some auxiliary definitions to this effect.
 
 (defun (function? x)
@@ -185,7 +190,7 @@
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Part 3: Streams + Unification = Goals
+; Part 3: Goals as Streams of Unification Results
 
 ; Part 1 mentions that the miniKanren family of logic languages works by manipulating streams.
 ; But, streams of what? Streams of environments from Part 2, of course!
@@ -216,6 +221,9 @@
 		  (` ()))))
       ((((_ . Y) . 3) ((_ . X) . 2))))
 
+; ^ Notice that we "seed" our goal with a stream containing one empty environment to "run" it.
+; We could also manually specify a stream of environments to try:
+
 (test (take () (let ((X (var 'X)) (Y (var 'Y)))
 	   ((== (` 5 , X 7 8) (` 5 6 , Y 8))
 	    (` ((, X . 4))
@@ -227,12 +235,22 @@
       ((((_ . Y) . 7) ((_ . X) . 6))))
 
 
-; One goal is not much of a logic program, however.
-; To combine goals, we have two obvious connectives:
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Part 4: Logic Programs as Combinations of Goals
+
+; But one goal is still not much of a logic program.
+
+; Our goals take a stream of environments and produce a new stream of environments,
+; so it should be possible to join them together to make more interesting programs.
+
+; Goals and environments can be arbitrarily complex in principle.
+; TODO: Leverage this flexibility to try to implement constraint logic programming.
+
+; But to start with, we can at least implement two simple logical connectives:
 ; * Conjunction (logical AND)
 ; * Disjunction (logical OR)
 
-; Thankfully, these can be implemented intuitively with what we have.
+; Thankfully, these are intuitive with what we have.
 
 ; Conjunction: Pass the stream through one goal, then the other.
 ; This requires each environment in the output stream to pass through BOTH goals.
@@ -265,4 +283,103 @@
        (((_ . Y) . 3) ((_ . X) . 2))
        (((_ . Y) . 4) ((_ . X) . 2))))
 
-; TODO: conde, fresh, reify, run
+; Remember that we can run our logic program by passing it a stream containing an empty environment.
+; This will become relevant again in just a moment...
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Part 5: Improving the User Interface
+
+; We almost have a very small logic programming language working now.
+; The last few issues are mostly a matter of convenience.
+
+; First issue: Manually chaining `conj` and `disj` isn't particularly convenient.
+; We can define the `cond`-like `conde` macro to make our lives easier.
+
+; First, a helper function to place the operators:
+(defun (chain op)
+  (lambda (forms)
+    ; (a b c d) => (op a (op b (op c d)))
+    (fold-right
+      (lambda (clause x)
+	(if (not x) clause
+	  (list op clause x)))
+      ()
+      forms)))
+
+; Now for `conde` itself:
+(defmacro (conde . forms)
+  ; (conde (a b) (c d)) => (disj (conj a b) (conj c d))
+  ((chain 'disj) (map (chain 'conj) forms)))
+
+(test (expand '(conde (a b c) (d e f)))
+      (disj (conj a (conj b c)) (conj d (conj e f))))
+
+; Second issue: Having to manually instantiate new variables with `let` and `var` is verbose.
+; We can introduce a more compact notation with `fresh`.
+
+(defmacro (fresh vars . body)
+  ; (fresh (a b) c d) => (let ((a (var)) (b (var))) (conj c d))
+  (` let , (map (lambda (v) (` , v (var (quote , v)))) vars)
+     , ((chain 'conj) body)))
+
+(test (expand '(fresh (a b c) d))
+      ((lambda (a b c) d) (var (quote a)) (var (quote b)) (var (quote c))))
+
+(test (expand '(fresh (a b c) d e))
+      ((lambda (a b c) (conj d e)) (var (quote a)) (var (quote b)) (var (quote c))))
+
+; Third issue: Reading the results from our logic programs may become difficult.
+; Usually, we have one kind of result that we want to see.
+; But that result might be an object represented by many different variable assignments.
+
+; First, let's turn an object from the environment into a real "thing" for us to look at.
+; In logic programming, this is called "reification." (Ultimately from rēs "thing" + faciō "make")
+(defun (reify v e)
+  (let ((x (walk v e)))
+    (cond ((var? x) x)
+	  ((atom x) x)
+	  (t (cons (reify (car x) e)
+		   (reify (cdr x) e))))))
+
+(test (fresh (X Y Z) (reify Z (list (cons X 'a) (cons Y 'b) (cons Z (cons X Y)))))
+      (a . b))
+
+(test (fresh (X Y Z) (reify Z (list (cons X 'a) (cons Z (cons X Y)))))
+      (a _ . Y))
+
+; But our logic programs return not one environment, but a stream of environments.
+; This makes it awkward to call `reify` directly, since we have to `take` first.
+; And before we can `take`, we have to "seed" our program with an empty environment.
+
+; We remedy both annoyances simultaneously with a simple `run` macro.
+
+(defun (reify-all q es)
+  (map (lambda (e) (reify q e)) es))
+
+(defmacro (run n q g)
+  (` fresh (, q)
+     (reify-all , q (take , n (, g '(()))))))
+
+(test (run () Q (conde ((== Q 5)) ((== Q 6) (== Q 7)) ((== Q 8))))
+      (5 8))
+
+
+; Now for a programming example:
+
+(define appendo
+  (lambda (X Y Z)
+    (lambda (s)
+      (lambda ()
+	((fresh
+	   (X0 Xs Zs)
+	   (conde ((== X ())
+		   (== Y Z))
+		  ((== X (cons X0 Xs))
+		   (== Z (cons X0 Zs))
+		   (appendo Xs Y Zs))))
+	 s)))))
+
+(run 6 Q (fresh (A B) (appendo A B '(a b c d e)) (== Q (list A B))))
+; ^ TODO: Why do (run () ...) and (run 7 ...) not terminate here?
+; ^ TODO: `relation` macro
